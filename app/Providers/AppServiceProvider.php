e9eec9323b76d0616585ca656fb69e8e0e4a3b86
iawavgvst <alkelin1@g,ail.com>
2026-02-07 10:18:58 +0300

    policy (book, user)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Book;
+use App\Models\User;
+use App\Policies\BookPolicy;
+use App\Policies\UserPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Book::class, BookPolicy::class);
+        Gate::policy(User::class, UserPolicy::class);
     }
 }
